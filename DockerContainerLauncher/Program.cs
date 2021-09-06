e4a3e0a687ffb0f2e0c3eaa0c0e01059b409fe29
Nova <nova.wiffen@gmail.com>
2021-09-06 21:17:57 +0200

    initial commit

---
+++ DockerContainerLauncher/Program.cs
@@ -0,0 +1,24 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerContainerLauncher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DockerClient client = new DockerClientConfiguration(
+                new Uri("unix:///var/run/docker.sock"))
+                .CreateClient();
+
+            IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
+                new ContainersListParameters(){
+                    Limit = 10,
+                });
+        }
+    }
+}
